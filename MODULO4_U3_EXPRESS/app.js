20cd103179c85cada4f1cd486403adf07c188361
Fedberen <95238263+Fedberen@users.noreply.github.com>
2022-04-07 16:51:32 -0300

    Add files via upload
    Tarea de la unidad 3 del Modulo4

---
+++ MODULO4_U3_EXPRESS/app.js
@@ -0,0 +1,49 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var solarpageRouter = require('./routes/solarpage'); //Solar
+var explorepageRouter = require('./routes/explorepage'); //Explore
+var lifepageRouter = require('./routes/lifepage'); //Life
+var contactopageRouter = require('./routes/contactopage'); //Contacto
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/solarpage', solarpageRouter);
+app.use('/lifepage', lifepageRouter);
+app.use('/contactopage', contactopageRouter);
+app.use('/explorepage', explorepageRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
