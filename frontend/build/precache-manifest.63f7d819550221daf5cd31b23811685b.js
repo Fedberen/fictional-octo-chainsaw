f3865fbb42906e3db802d19af88e51331224c394
Fedberen <95238263+Fedberen@users.noreply.github.com>
2022-05-06 21:48:43 -0300

    Add files via upload

---
+++ frontend/build/precache-manifest.63f7d819550221daf5cd31b23811685b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "50c2d21a0b08738a79ba",
+    "url": "/static/css/main.82a96102.chunk.css"
+  },
+  {
+    "revision": "50c2d21a0b08738a79ba",
+    "url": "/static/js/main.50c2d21a.chunk.js"
+  },
+  {
+    "revision": "2c80ac1fe2aa0d16567f",
+    "url": "/static/js/1.2c80ac1f.chunk.js"
+  },
+  {
+    "revision": "e47ac32be86b71b6c4d9",
+    "url": "/static/js/2.e47ac32b.chunk.js"
+  },
+  {
+    "revision": "b7a5294dd25a4e7d9fd9",
+    "url": "/static/js/runtime~main.b7a5294d.js"
+  },
+  {
+    "revision": "a528d722e1724aafb4ffb35aed67e4a9",
+    "url": "/index.html"
+  }
+];
